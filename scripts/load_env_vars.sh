7ccca2bb3111f142e8d650277149b9dda5875bed
Erez Alfasi <ereza@mellanox.com>
2017-09-14 14:57:19 +0300

    Senty configuration: add Senty configuration files
    Add senty configuration files
    
    Signed-off-by: Erez Alfasi <ereza@mellanox.com>

---
+++ scripts/load_env_vars.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export VIRTUAL_ENV=/auto/integration/Utilities/avocado_framework/avocado
+export PYTHONPATH=/auto/integration/Utilities/avocado_framework/avocado/usr/lib64/python2.6/site-packages:/auto/integration/Utilities/avocado_framework/avocado/usr/lib/python2.6/site-packages/
+export PATH=/auto/integration/Utilities/avocado_framework/avocado/usr/bin/:$PATH
